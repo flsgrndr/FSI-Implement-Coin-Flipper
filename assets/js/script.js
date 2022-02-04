da77892dcf9d8eed92f80ed50eee56e40fb68dfc
unknown <jam319@protonmail.com>
2022-02-03 20:43:19 -0500

    completed according to basic instructions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,66 @@
-// TODO: Declare any global variables we need
+let numberOfHeads = 0
+let numberOfTails = 0
+let headsPercentage = 0
+let tailsPercentage = 0
+let totalFlips = numberOfHeads + numberOfTails
 
+let images = [
+    {
+        imagePath: './assets/images/penny-heads.jpg',
+        side: 'heads',
+        message: 'You flipped heads!'
+    },
+    {
+        imagePath: './assets/images/penny-tails.jpg',
+        side: 'tails',
+        message: 'You flipped tails!'
+    }
+]
 
 document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
 
-    // TODO: Add event listener and handler for flip and clear buttons
+    document.getElementById('flip').addEventListener('click', function(){
 
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
+        let randomImage = images[Math.floor(Math.random() * images.length)]
 
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
+        document.getElementById('penny-img').src = randomImage.imagePath
+        document.querySelector('.message-container').textContent = randomImage.message
 
+        if(randomImage.side === 'heads') {
 
-    // Clear Button Click Handler
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
+            numberOfHeads ++
+            document.getElementById('heads').textContent = numberOfHeads
+
+        } else {
+
+            numberOfTails ++
+            document.getElementById('tails').textContent = numberOfTails
+
+        }
+
+        totalFlips++
+
+        headsPercentage = Math.round(numberOfHeads / totalFlips * 100)
+        tailsPercentage = Math.round(numberOfTails / totalFlips * 100)
+
+        document.getElementById('heads-percent').textContent = headsPercentage + '%'
+        document.getElementById('tails-percent').textContent = tailsPercentage + '%'
+
+    })
+
+    document.getElementById('clear').addEventListener('click', function(){
+
+        numberOfHeads = 0
+        numberOfTails = 0
+        headsPercentage = 0
+        tailsPercentage = 0
+        totalFlips = 0
+        
+        document.getElementById('heads').textContent = 0
+        document.getElementById('tails').textContent = 0
+        document.getElementById('heads-percent').textContent = 0 + '%'
+        document.getElementById('tails-percent').textContent = 0 + '%'
+        
+    })
 
 })
